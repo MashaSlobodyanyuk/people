c05970e957cd3240174b72a569fd01c77aa818ab
Masha S <mashaslobodyanyuk@gmail.com>
2023-06-11 01:09:03 +0300

    Correct

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 fun main() {
 
-    var likes = 11
+    var likes = 111
 
 
-    val person = if ((likes % 10).equals(1) && (!(likes % 100).equals(11))){
+    val person = if (likes % 10 == 1 && likes % 100 != 11){
         println("Понравилось $likes человеку")
     } else{
         println("Понравилось $likes людям")
